09d43e305caa588cfa8dfbd3dd7e5d264a023c81
Benjamin <72185664+benjaminfrancis@users.noreply.github.com>
2026-02-09 17:15:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,6 +16,20 @@ resource "google_compute_subnetwork" "subnet" {
   network       = google_compute_network.vpc_network.id
 }
 
+resource "google_compute_router" "router" {
+  name    = "apache-router"
+  network = google_compute_network.vpc_network.id
+  region  = var.region
+}
+
+resource "google_compute_router_nat" "nat" {
+  name                               = "apache-nat"
+  router                             = google_compute_router.router.name
+  region                             = var.region
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+}
+
 resource "google_compute_instance" "apache" {
   name         = "apache-webserver"
   machine_type = "e2-micro"
@@ -30,7 +44,7 @@ resource "google_compute_instance" "apache" {
   network_interface {
     network    = google_compute_network.vpc_network.id
     subnetwork = google_compute_subnetwork.subnet.id
-    access_config {}
+    # No access_config = no external IP (required by org policy)
   }
 
   metadata_startup_script = <<-EOT
@@ -44,7 +58,7 @@ resource "google_compute_instance" "apache" {
   tags = ["http-server"]
 }
 
-resource "google_compute_firewall" "default" {
+resource "google_compute_firewall" "allow_http" {
   name    = "allow-http"
   network = google_compute_network.vpc_network.id
 
@@ -55,4 +69,71 @@ resource "google_compute_firewall" "default" {
 
   source_ranges = ["0.0.0.0/0"]
   target_tags   = ["http-server"]
+}
+
+resource "google_compute_firewall" "allow_health_check" {
+  name    = "allow-health-check"
+  network = google_compute_network.vpc_network.id
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+  source_ranges = ["35.191.0.0/16", "130.211.0.0/22"]
+  target_tags   = ["http-server"]
+}
+
+resource "google_compute_global_address" "default" {
+  name = "apache-lb-ip"
+}
+
+resource "google_compute_health_check" "default" {
+  name               = "apache-health-check"
+  check_interval_sec = 5
+  timeout_sec        = 5
+
+  http_health_check {
+    port = 80
+  }
+}
+
+resource "google_compute_instance_group" "apache" {
+  name      = "apache-instance-group"
+  zone      = var.zone
+  instances = [google_compute_instance.apache.id]
+
+  named_port {
+    name = "http"
+    port = "80"
+  }
+}
+
+resource "google_compute_backend_service" "default" {
+  name          = "apache-backend"
+  health_checks = [google_compute_health_check.default.id]
+  port_name     = "http"
+  protocol      = "HTTP"
+  timeout_sec   = 10
+
+  backend {
+    group = google_compute_instance_group.apache.id
+  }
+}
+
+resource "google_compute_url_map" "default" {
+  name            = "apache-url-map"
+  default_service = google_compute_backend_service.default.id
+}
+
+resource "google_compute_target_http_proxy" "default" {
+  name    = "apache-http-proxy"
+  url_map = google_compute_url_map.default.id
+}
+
+resource "google_compute_global_forwarding_rule" "default" {
+  name       = "apache-forwarding-rule"
+  target     = google_compute_target_http_proxy.default.id
+  port_range = "80"
+  ip_address = google_compute_global_address.default.id
 }
