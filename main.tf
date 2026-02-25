add5743ea39587f1c568c0820adf32c5c0e7c5e6
Benjamin <72185664+benjaminfrancis@users.noreply.github.com>
2026-02-25 22:53:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -35,7 +35,11 @@ resource "google_secret_manager_secret" "ansible_private_key" {
   secret_id = "ansible-ssh-private-key"
 
   replication {
-    auto {}
+    user_managed {
+      replicas {
+        location = var.region
+      }
+    }
   }
 
   depends_on = [google_project_service.secretmanager]
@@ -100,11 +104,18 @@ resource "google_compute_router" "router" {
 
 # Cloud NAT for outbound internet access (package downloads, updates)
 resource "google_compute_router_nat" "nat" {
-  name                               = "ansible-demo-nat"
-  router                             = google_compute_router.router.name
-  region                             = var.region
-  nat_ip_allocate_option             = "AUTO_ONLY"
-  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+  name   = "ansible-demo-nat"
+  router = google_compute_router.router.name
+  region = var.region
+
+  nat_ip_allocate_option = "AUTO_ONLY"
+  
+  source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
+  
+  subnetwork {
+    name                    = google_compute_subnetwork.subnet.id
+    source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
+  }
 
   log_config {
     enable = true
