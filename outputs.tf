28a576288817528c8d3b0762383b8cf06ee6fa36
Benjamin <72185664+benjaminfrancis@users.noreply.github.com>
2026-02-25 16:16:16 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,6 @@
-output "instance_public_ip" {
-  description = "Public IP of the GCE instance"
-  value       = google_compute_instance.demo_instance.network_interface[0].access_config[0].nat_ip
-}
-
-output "secret_name" {
-  description = "Secret Manager secret name containing SSH private key"
-  value       = google_secret_manager_secret.ansible_private_key.secret_id
+output "instance_internal_ip" {
+  description = "Internal IP of the GCE instance"
+  value       = google_compute_instance.demo_instance.network_interface[0].network_ip
 }
 
 output "instance_name" {
@@ -16,4 +11,14 @@ output "instance_name" {
 output "instance_zone" {
   description = "Zone of the compute instance"
   value       = google_compute_instance.demo_instance.zone
+}
+
+output "secret_name" {
+  description = "Secret Manager secret name containing SSH private key"
+  value       = google_secret_manager_secret.ansible_private_key.secret_id
+}
+
+output "iap_tunnel_command" {
+  description = "Command to create IAP SSH tunnel for accessing the instance"
+  value       = "gcloud compute start-iap-tunnel ${google_compute_instance.demo_instance.name} 80 --local-host-port=localhost:8080 --zone=${var.zone} --project=${var.project}"
 }
